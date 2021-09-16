cf846025eec76adf44d1fae8a38813d81ba45478
odeljs <odel.justin@gmail.com>
2021-09-16 15:49:20 -0400

    Addition of CSV file creation and population of faulty links

--- Website-Link-Validation-Tool/src/linkValidation/HrefValidation.java
+++ Website-Link-Validation-Tool/src/linkValidation/HrefValidation.java
@@ -1,7 +1,14 @@
 package linkValidation;
 
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -12,15 +19,37 @@ public class HrefValidation {
 	
 	
 	
-	public static void main(String[] args) {
+	
+	
+	
+	
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		//boolean checker,true = active false = not active
+		boolean activeChecker = true;
 		
 		//list that holds urls to be checked
 		LinkedList<String> URLList = new LinkedList<>();
 		
+		System.setProperty("webdriver.chrome.driver","/Users/odelj/Selenium/chromedriver");
 		
 		WebDriver driver = new ChromeDriver();
 		
 		driver.manage().window().maximize();
+		
+		//CSV file
+		File csvFile = new File("Today.csv");
+		
+		PrintWriter out = new PrintWriter(csvFile);
+		
+		Date date = Calendar.getInstance().getTime();  
+		
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");  
+		
+		String currentDate = dateFormat.format(date);  
+		
+		
 		
 		//Get URLs to pages from file
 		try {
@@ -54,10 +83,83 @@ public class HrefValidation {
 			
 			//get all href elements on the driver webpage
 			List<WebElement> links = driver.findElements(By.tagName("a"));
+		
+			
+			
+			//iterate through all links on page
+			for(int i = 0; i < links.size(); i++) {
+				
+				//get the first href element
+				WebElement currentElement = links.get(i);
+				
+				//create string of href element to pass to verifier method
+				String currentHref = currentElement.getAttribute("href");
+			
+				//if null skip iteration
+				if(currentHref == null) {
+					continue;
+				}
+				
+				//if current URL contains http then verify
+				if(currentHref.contains("http")) {
+					
+					//call method to verify currentURL
+					activeChecker = verifyLinkIsActive(currentHref);
+				}
+				
+				//if activeLinkChecker is false it is a bad link so input it and the webpage its
+				//on into csv file
+				
+				if(activeChecker == false) {
+					out.printf("%s, %s, %s", currentDate, URLList.getFirst(), currentHref);
+				}
+				
+			} 
+		
+			//remove the already checked URL webpage from the list
+			URLList.removeFirst();
+			
+			//remove all items from href list
+			links.clear();
+			
+		
+		
 		}
 	
+	out.close();
 	
 		
 	
+	}
+	
+	
+	public static boolean verifyLinkIsActive(String currentHref)  {
+		
+		boolean activeChecker = true;
+		
+		  try 
+	        {
+	           URL url = new URL(currentHref);
+	           
+	           HttpURLConnection httpURLConnect=(HttpURLConnection)url.openConnection();
+	           
+	           httpURLConnect.setConnectTimeout(3000);
+	           
+	           httpURLConnect.connect();
+	           
+	           if(httpURLConnect.getResponseCode()==200)
+	           {
+	               activeChecker = true;
+	        	   System.out.println(currentHref+" - "+httpURLConnect.getResponseMessage());
+	            }
+	          if(httpURLConnect.getResponseCode()==HttpURLConnection.HTTP_NOT_FOUND)  
+	           {
+	              activeChecker = false; 
+	        	  System.out.println(currentHref+" - "+httpURLConnect.getResponseMessage() + " - "+ HttpURLConnection.HTTP_NOT_FOUND);
+	            }
+	        } catch (Exception e) {
+	           
+	        }
+		  return activeChecker;
 	}
 }
